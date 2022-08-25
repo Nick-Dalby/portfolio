23a055fd9865667de27e64c9290afaa8678c975b
Nick Dalby <nckdlby@gmail.com>
2022-08-25 15:08:46 +0200

    added: time widget using luxon library

---
+++ src/scripts/times.js
@@ -0,0 +1,19 @@
+import { DateTime } from "luxon";
+
+const clock = document.querySelector('.clock')
+
+const updateTime = () => {
+  const now = DateTime.now().setZone("Europe/Berlin")
+  // console.log(now);
+  const output = document.querySelector('.time')
+  output.innerText = now.toFormat('HH:mm:ss')
+
+  const hour = parseInt(now.toFormat('H'))
+  if (hour >= 9 && hour < 18) {
+  clock.classList.add('open')
+  }
+}
+
+setInterval(() => {
+  updateTime()
+},1000)
