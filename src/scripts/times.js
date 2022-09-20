74e785d3c58117ee63c528a1afd1ac0620bb3fc4
Nick Dalby <nckdlby@gmail.com>
2022-09-20 10:58:02 +0200

    edit: tweets

--- src/scripts/times.js
+++ src/scripts/times.js
@@ -1,15 +1,15 @@
-import { DateTime } from "luxon";
+import { DateTime } from 'luxon'
 
 const clock = document.querySelector('.clock')
 
 const updateTime = () => {
-  const now = DateTime.now().setZone("Europe/Berlin")
+  const now = DateTime.now().setZone('Europe/Berlin')
   const output = document.querySelector('.time')
   output.innerText = now.toFormat('HH:mm:ss')
 
   const hour = parseInt(now.toFormat('H'))
   if (hour >= 9 && hour < 18) {
-  clock.classList.add('open')
+    clock.classList.add('open')
   }
 }
 
